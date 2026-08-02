5a03f25ed31fbb9efbe78e97ddb63b6c46e29af3
Calin Luchian <calin.luchian@gmail.com>
2026-08-02 12:16:40 +0300

    Formated file

--- src/main.ts
+++ src/main.ts
@@ -4,48 +4,50 @@ let timerReset: boolean = true;
 const timeout: number = 1000;
 const min_timer = 5;
 const max_timer = 10;
-const togglePlay = (audio: { paused: boolean; play: () => void; pause: () => void; }) => audio.paused ? audio.play() : audio.pause();
+const togglePlay = (audio: {
+  paused: boolean;
+  play: () => void;
+  pause: () => void;
+}) => (audio.paused ? audio.play() : audio.pause());
 
-function toggleLoading(): void{
+function toggleLoading(): void {
   const load_logo = document.getElementById("load-cat");
   const dance_cat = document.getElementById("cat-container");
   const refresh_btn = document.getElementById("refresh-btn");
   const loading_text = document.getElementById("loading-text");
   const title_message = document.getElementById("title-text");
-  const slots_audio = <HTMLAudioElement>document.getElementById("myAudio")
-  playAudio(slots_audio)
-  toggleVisibility(load_logo)
-  toggleVisibility(dance_cat)
-  toggleVisibility(refresh_btn)
-  toggleVisibility(loading_text)
-  refreshTitle()
-  toggleShake(title_message)
-
+  const slots_audio = <HTMLAudioElement>document.getElementById("myAudio");
+  playAudio(slots_audio);
+  toggleVisibility(load_logo);
+  toggleVisibility(dance_cat);
+  toggleVisibility(refresh_btn);
+  toggleVisibility(loading_text);
+  refreshTitle();
+  toggleShake(title_message);
 }
 
 function playAudio(element: HTMLAudioElement | null) {
-  if (!element) return
+  if (!element) return;
   element.volume = 0.05;
-  togglePlay(element)
+  togglePlay(element);
+  togglePlay(element);
 }
 
-function refreshTitle(): void{
-  const title_container = document.getElementById("title-text")
+function refreshTitle(): void {
+  const title_container = document.getElementById("title-text");
   if (title_container) {
-    if (timerReset)
-      title_container.innerText = "Beb Moments in Progress"
-    else
-      title_container.innerText = "Dai mesaj lui BEB!!!!"
+    if (timerReset) title_container.innerText = "Beb Moments in Progress";
+    else title_container.innerText = "Dai mesaj lui BEB!!!!";
   }
 }
 
-function restart_timer(): void{
-  timerReset = true
-  toggleLoading()
-  startTimer()
+function restart_timer(): void {
+  timerReset = true;
+  toggleLoading();
+  startTimer();
 }
 
-function incrementText(): void{
+function incrementText(): void {
   globalCounter = (globalCounter + 1) % 4;
   const baseText = "Loading! Please wait";
   const textContainer = document.getElementById("loading-text");
@@ -54,42 +56,38 @@ function incrementText(): void{
   textContainer.innerHTML = baseText + ".".repeat(globalCounter);
 }
 
-function danceCat() : void {
-  timerReset = false
-  toggleLoading()
-} 
+function danceCat(): void {
+  timerReset = false;
+  toggleLoading();
+}
 
 function startTimer(): void {
-  
   randomTimer = newTimerValue(min_timer, max_timer) * 1000;
-  window.setTimeout(danceCat, randomTimer)
-  
+  window.setTimeout(danceCat, randomTimer);
 }
 
 function toggleVisibility(element: HTMLElement | null) {
-  if(!element) return
+  if (!element) return;
   element.classList.toggle("hidden");
   console.log("Toggled element " + element.id);
 }
 
 function toggleShake(element: HTMLElement | null) {
-  if(!element) return
+  if (!element) return;
   element.classList.toggle("shake-element");
   console.log("Toggled element " + element.id);
 }
 
-const newTimerValue = (min: number, max : number) => {
-   return Math.floor(Math.random() * (max - min) ) + min;
-}
+const newTimerValue = (min: number, max: number) => {
+  return Math.floor(Math.random() * (max - min)) + min;
+};
 
 const button = document.getElementById("refresh-btn");
-button?.addEventListener("click", () => restart_timer())
-
-
+button?.addEventListener("click", () => restart_timer());
 
 window.addEventListener("DOMContentLoaded", () => {
   startTimer();
-  console.log("Timer is " + randomTimer);  
+  console.log("Timer is " + randomTimer);
 });
 
-window.setInterval(incrementText, timeout)
+window.setInterval(incrementText, timeout);
