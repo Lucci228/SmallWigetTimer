9edd595442d3244ef5673829b0c5bd57049cc305
Lucci228 <151786062+Lucci228@users.noreply.github.com>
2026-08-14 16:00:55 +0300

    Added animations

--- src/components/popup/popup.ts
+++ src/components/popup/popup.ts
@@ -4,3 +4,8 @@ const close_btn = document.getElementById("close-btn");
 close_btn?.addEventListener("click", async () => {
   await getCurrentWindow().close();
 })
+const wrong_btn = document.getElementById("wrong-btn");
+wrong_btn?.addEventListener("click", () => {
+  wrong_btn.remove()
+})
+wrong_btn?.onclick
